1db2e30826231baf0f91ecf6ce06c856ca175e60
thanhle0203 <thanhlevan0203@gmail.com>
2022-09-17 15:41:08 -0700

    Created Add Cart functinality

--- Shopping/index.php
+++ Shopping/index.php
@@ -5,33 +5,35 @@
     // create instance of db class
     $database = new CreateDb("Productdb", "Producttb");
 
-    if(isset($_POST['add'])) {
-        if (isset($_SESSION['cart'])) {
+    if (isset($_POST['add'])){
+        /// print_r($_POST['product_id']);
+        if(isset($_SESSION['cart'])){
+    
             $item_array_id = array_column($_SESSION['cart'], "product_id");
-            print_r($item_array_id);
-
+    
             if(in_array($_POST['product_id'], $item_array_id)){
-                echo "<script>alert('Product is already added to the cart...!')</script>";
+                echo "<script>alert('Product is already added in the cart..!')</script>";
                 echo "<script>window.location = 'index.php'</script>";
-            } else {
-                $count($_SESSION['cart']);
+            }else{
+    
+                $count = count($_SESSION['cart']);
                 $item_array = array(
                     'product_id' => $_POST['product_id']
                 );
-
+    
                 $_SESSION['cart'][$count] = $item_array;
-                print_r($_SESSION['cart']);
-
             }
-        } else {
+    
+        }else{
+    
             $item_array = array(
-                'product_id' => $_POST['product_id']
+                    'product_id' => $_POST['product_id']
             );
+    
+            // Create new session variable
+            $_SESSION['cart'][0] = $item_array;
+            print_r($_SESSION['cart']);
         }
-
-        // Create new session variable
-        $_SESSION['cart'][0] = $item_array;
-        print_r($_SESSION['cart']);
     }
     
 ?>
