8c567b5df112c7054c224c56f11eda66c429893c
thanhle0203 <thanhlevan0203@gmail.com>
2022-09-17 15:45:23 -0700

    Created Shopping Cart Navbar

---
+++ Shopping/php/header.php
@@ -0,0 +1,8 @@
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"></nav>
+    <a href="index.php" class="navbar-brand">
+        <p3>
+            <i class="fas fa-shopping-basket"></i>Shopping Cart
+        </p3>
+    </a>
+</header>
