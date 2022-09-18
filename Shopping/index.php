25df2f682fb050d1bcf3d912395346e07cd533c7
thanhle0203 <thanhlevan0203@gmail.com>
2022-09-17 23:38:36 -0700

    Created Cart Page

--- Shopping/index.php
+++ Shopping/index.php
@@ -8,27 +8,20 @@
     if (isset($_POST['add'])){
         /// print_r($_POST['product_id']);
         if(isset($_SESSION['cart'])){
-    
-            $item_array_id = array_column($_SESSION['cart'], "product_id");
-    
-            if(in_array($_POST['product_id'], $item_array_id)){
-                echo "<script>alert('Product is already added in the cart..!')</script>";
-                echo "<script>window.location = 'index.php'</script>";
-            }else{
-    
+            $item_array_id = array_column($_SESSION['cart'], 'product_id');
+            //$_SESSION['cart']['product_id'] = $count;
+            if(!in_array($_POST['product_id'], $item_array_id)){
                 $count = count($_SESSION['cart']);
-                $item_array = array(
-                    'product_id' => $_POST['product_id']
-                );
-    
+                $item_array = array('product_id' => $_POST['product_id']);
                 $_SESSION['cart'][$count] = $item_array;
+            }else{
+                //echo "<script>window.location = 'index.php'</script>";
+                echo "<script>alert('Product is already added in the cart..!')</script>";
+                //echo "<script>window.location = 'index.php'</script>";
             }
     
         }else{
-    
-            $item_array = array(
-                    'product_id' => $_POST['product_id']
-            );
+            $item_array = array('product_id' => $_POST['product_id']);
     
             // Create new session variable
             $_SESSION['cart'][0] = $item_array;
@@ -53,7 +46,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
     
     <!-- Font Awesome -->
-    <!-- <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">  -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">  
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
--- Shopping/php/header.php
+++ Shopping/php/header.php
@@ -1,31 +1,44 @@
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a href="index.php" class="navbar-brand">
-        <p3>
-            <i class="fas fa-shopping-basket"></i>Shopping Cart
-        </p3>
-    </a>
+        <a href="index.php" class="navbar-brand">
+            <p3 class="px-5>
+                <i class="fas fa-shopping-basket"></i>Shopping Cart
+            </p3>
+        </a>
 
-    <button class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-    >
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <button class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="mr-auto"></div>
-        <div class="navbar-nav">
-            <a href="cart.php" class="nav-item nav-link active">
-                <h5 class="px-5 cart">
-                    <i class="fas fa-shopping-cart"></i>Cart
-                    <span id="cart_count">0</span>
-                </h5>
-            </a>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="mr-auto"></div>
+            <div class="navbar-nav">
+                <a href="cart.php" class="nav-item nav-link active">
+                    <h5 class="px-5 cart">
+                        <i class="fas fa-shopping-cart"></i>Cart
+
+                        <?php
+                        // <span id="cart_count" class="text-warning bg-light">0</span>
+                        if (isset($_SESSION['cart'])){
+                            $count = count($_SESSION['cart']);
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">$count</span>";
+                        }else{
+                            echo "<span id=\"cart_count\" class=\"text-warning bg-light\">0</span>";
+                        }
+
+                        ?>
+
+                    </h5>
+                </a>
+            </div>
         </div>
-    </div>
+
+    </nav>
 </header>
